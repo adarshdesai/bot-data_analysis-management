2af2f628104550fbe323094d83f98e3f78f82672
moumita-das <mdas.160395@gmail.com>
2017-10-25 11:01:30 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os.path
 import sys
 import json
-from backend_functions import speech
+from backend_functions import speech, nameSearch, name
 try:
     import apiai
 except ImportError:
@@ -15,8 +15,11 @@ CLIENT_ACCESS_TOKEN = '7df34bbf717b412d97b7056c110b967e'
 
 def main():
 	 apiIntent=""
-	 while(1 and apiIntent!="bye"):
-	    stringu=raw_input()
+	 while(apiIntent!="bye"):
+	    stringu=raw_input("User : ")
+	    stringu=speech.punctuate(stringu)
+	    userSentiment=str(speech.sentiment(stringu))
+	    print ("User Sentiment : "+userSentiment)
 	    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
 	    request = ai.text_request()
@@ -32,10 +35,18 @@ def main():
 	    string = response.read().decode('utf-8')
 	    json_obj = json.loads(string)
 	    apiIntent=json_obj["result"]["metadata"]["intentName"]
-	    if(apiIntent=="nameIntent"):
-		username=
-	    apiResp=json_obj["result"]["fulfillment"]["speech"]
+	    if(apiIntent=="find-name" or apiIntent=="nameIntent"):
+		stringu[0]=stringu[0].upper()
+		for i in range(1,len(stringu)):
+			if(stringu[i-1]==" "):
+				stringu[i]=stringu[i].upper()
+		username=name.find_name(stringu)
+		apiResp="Hello "+username+"! How may I help you?"
+	    else:
+	    	apiResp=json_obj["result"]["fulfillment"]["speech"]
+	    #print("Intent name: "+apiIntent)
 	    speech.say(apiResp)
+	    print("Agent : "+apiResp)
 
 if __name__ == '__main__':
 	main()
