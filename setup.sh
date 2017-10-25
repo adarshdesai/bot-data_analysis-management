bc29e0360d62f0772e906e3dafa56d90de8c6ae5
moumita-das <mdas.160395@gmail.com>
2017-10-25 09:09:01 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-sudo pip install -r requirement.text
+sudo pip install -r requirements.txt
+sudo apt-get install python-pyaudio
 sudo tar -xvf /home/bot-data_analysis-management/data/trans.csv.tar.gz
